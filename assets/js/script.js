2efc71b9ba64dff6c86d32f17560fdc6f1ba70ad
Eki Alfani <ekialfani15@gmail.com>
2022-09-04 11:19:11 +0800

    add: 'RENDER_EVENT' to put books on the bookshelf
    books that have been read will be place in the completed-bookshelf and vice versa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,55 @@
 const bookList = [];
+const RENDER_EVENT = 'render-book';
 
-function createBookDataObject(id, title, published, isCompleted) {
+
+function createBookItem(bookData){
+	const bookTitle = document.createElement('h3');
+	bookTitle.innerText = bookData.title;
+
+	const author = document.createElement('p');
+	author.innerText = 'Penulis: ' +bookData.author;
+
+	const published = document.createElement('p');
+	published.innerText = 'Tahun Terbit: ' +bookData.published;
+
+	const container = document.createElement('li');
+	container.classList.add('book-item');
+
+	container.append(bookTitle, author, published);
+
+	return container;
+}
+
+
+function renderBookList() {
+	const uncompletedBookList = document.getElementById('uncompleted-list');
+	const completedBookList = document.getElementById('completed-list')
+	
+	uncompletedBookList.innerHTML = '';
+	completedBookList.innerHTML = '';
+
+	for(const book of bookList){
+		const bookItem = createBookItem(book);
+
+		if(!book.isCompleted){
+			uncompletedBookList.append(bookItem);
+		}else {
+			completedBookList.append(bookItem);
+		}
+	}
+}
+
+
+document.addEventListener(RENDER_EVENT, function(){
+	renderBookList();
+})
+
+
+function createBookDataObject(id, title, author, published, isCompleted) {
 	return {
 		id,
 		title,
+		author,
 		published,
 		isCompleted
 	}
@@ -14,6 +60,7 @@ function generateId() {
 	return +new Date();
 }
 
+
 function addNewBook(){
 	const title = document.getElementById('title').value;
 	const author = document.getElementById('author').value;
@@ -21,11 +68,12 @@ function addNewBook(){
 	const isCompleted = document.getElementById('is-completed').checked;
 
 	const id = generateId();
-	const bookData = createBookDataObject(id, title, published, isCompleted);
+	const bookData = createBookDataObject(id, title, author, published, isCompleted);
 
 	bookList.push(bookData);
-	console.log(bookList);
+	document.dispatchEvent(new Event(RENDER_EVENT));
 }
+
 
 document.addEventListener('DOMContentLoaded', function(){
 	const addBookForm = document.getElementById('add-book-form');
