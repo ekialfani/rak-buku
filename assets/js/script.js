f552b008fb739f0d67693a935d5046e55f07988f
Eki Alfani <ekialfani15@gmail.com>
2022-09-04 09:48:57 +0800

    convert: book data into objects and push to bookList array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,38 @@
+const bookList = [];
+
+function createBookDataObject(id, title, published, isCompleted) {
+	return {
+		id,
+		title,
+		published,
+		isCompleted
+	}
+}
+
+
+function generateId() {
+	return +new Date();
+}
+
+function addNewBook(){
+	const title = document.getElementById('title').value;
+	const author = document.getElementById('author').value;
+	const published = document.getElementById('published').value;
+	const isCompleted = document.getElementById('is-completed').checked;
+
+	const id = generateId();
+	const bookData = createBookDataObject(id, title, published, isCompleted);
+
+	bookList.push(bookData);
+	console.log(bookList);
+}
+
 document.addEventListener('DOMContentLoaded', function(){
 	const addBookForm = document.getElementById('add-book-form');
 
 	addBookForm.addEventListener('submit', function(e){
 		e.preventDefault();
 
-		const title = document.getElementById('title').value;
-		const author = document.getElementById('author').value;
-		const published = document.getElementById('published').value;
-		const isCompleted = document.getElementById('is-completed').checked;
-
-		console.log(title);
-		console.log(author);
-		console.log(published);
-		console.log(isCompleted);
+		addNewBook();
 	})
 })
