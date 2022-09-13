6ec0395cd1f4a36d1fdfc6798620772917604870
Eki Alfani <ekialfani15@gmail.com>
2022-09-13 13:04:52 +0800

    add: shadow to the header when page is scrolled

--- assets/js/script.js
+++ assets/js/script.js
@@ -331,3 +331,12 @@ searchInput.addEventListener('input', function(e){
 
 	searchBookByTitle(inputValue);
 })
+
+
+window.addEventListener('scroll', function(){
+	if(this.pageYOffset > 10){
+		document.querySelector('header').classList.add('shadow');
+	}else {
+		document.querySelector('header').classList.remove('shadow');	
+	}
+})
